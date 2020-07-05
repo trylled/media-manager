394285edd916cbf3885235227d6cd1f8a342c7c0
Trylle <tryllegoodiedeleon@gmail.com>
2020-07-05 19:22:38 +0800

    Skipping forward and back 10 seconds using ArrwoLeft and ArrowRight. #4

--- web-manager/ClientApp/src/app/media-view/media-view.component.ts
+++ web-manager/ClientApp/src/app/media-view/media-view.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectorRef, Component, Inject } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, Renderer2 } from '@angular/core';
 import { Subscription } from 'rxjs'
 import { HttpClient, HttpEvent } from '@angular/common/http';
 import { NotificationsService } from 'angular2-notifications';
 import * as _ from 'lodash';
+import { VgAPI } from 'videogular2/compiled/core';
 
 interface Media extends Selection {
   createdTimeStamp: string;
@@ -24,23 +25,40 @@ interface Selection {
 
 export class MediaViewComponent {
   private _httpClient: HttpClient;
+  private _keyEventListener: Function;
   private _notificationsService: NotificationsService
+  private _renderer: Renderer2
 
   private fileControllerUrl: string;
+  private isFetching: boolean;
   private uploadedMedia: Media[];
   private uploadFolder: string;
-  private isFetching: boolean;
+  private videoAPI: VgAPI;
 
   constructor(
     httpClient: HttpClient,
     private notificationsService: NotificationsService,
     private cdRef: ChangeDetectorRef,
+    private renderer: Renderer2,
     @Inject('BASE_URL') baseUrl: string) {
     this._httpClient = httpClient;
     this._notificationsService = notificationsService;
+    this._renderer = renderer;
     this.fileControllerUrl = baseUrl + 'file';
     this.uploadFolder = baseUrl + 'uploads/';
     this.fetchFiles();
+
+    this._keyEventListener = this._renderer.listen('document', 'keydown', (event) => {
+      if (!this.videoAPI) return;
+      switch (event.key) {
+        case 'ArrowLeft':
+          this.videoAPI.currentTime -= 10;
+          break;
+        case 'ArrowRight':
+          this.videoAPI.currentTime += 10;
+          break;
+      }
+    });
   }
 
   ngAfterViewChecked() {
@@ -58,5 +76,11 @@ export class MediaViewComponent {
       this.isFetching = false;
       this._notificationsService.info("Media Ready", "Media ready.");
     }, error => this._notificationsService.error("Error", error.message));
+  }
+
+  public fileClick(id: string, isSelected: boolean): void {
+    _.each(this.uploadedMedia, (m: Media) => {
+      m.isSelected = m.id === id ? isSelected : false;
+    })
   }
 }
